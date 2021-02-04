ce20ba260aa3551367330abb67b21a14649603d0
thesound666 <76500804+thesound666@users.noreply.github.com>
2021-02-04 18:56:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 #git clone https://github.com/rufengsuixing/luci-app-syncdial.git package/luci-app-syncdia
-git clone https://github.com/chenhaowen01/gdut-drcom-for-openwrt.git package/gdut-drcom-for-openwrt
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/MentoHUST-OpenWrt-ipk
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
